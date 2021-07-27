e006137f199300a120d2f59f217142a8d9bc7e0b
dungphamdev <58509770+dungphamdev@users.noreply.github.com>
2021-07-27 15:55:43 +0700

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,13 +36,13 @@ class LoginController extends Controller
     public function __construct()
     {
       
-        if (Auth::check() && Auth::user()->role->id == 1 ) {
-            $this->redirectTo = route('admin.dashboard');
-        }elseif(Auth::check() && Auth::user()->role->id == 2){
-                $this->redirectTo = route('landlord.dashboard');
-        }else{
-                $this->redirectTo = route('renter.dashboard');
-        }
-        $this->middleware('guest')->except('logout');
+        // if (Auth::check() && Auth::user()->role->id == 1 ) {
+        //     $this->redirectTo = route('admin.dashboard');
+        // }elseif(Auth::check() && Auth::user()->role->id == 2){
+        //         $this->redirectTo = route('landlord.dashboard');
+        // }else{
+        //         $this->redirectTo = route('renter.dashboard');
+        // }
+        // $this->middleware('guest')->except('logout');
     }
 }
